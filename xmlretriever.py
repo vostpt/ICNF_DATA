638a86b60d1f75b87942cdc63a08e96144e5e43a
Jorge Gomes <jorge.gomes@vost.pt>
2021-06-09 12:32:27 +0100

    Main app
    This app checks new entries on the INCF database and appends them to the current .csv file

---
+++ xmlretriever.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+# With thanks to https://stackoverflow.com/users/6792743/perl 
+
+# import libraries
+
+import pandas as pd 
+import requests
+import xml.etree.ElementTree as ET
+import io
+from io import StringIO  
+import time 
+
+# Get last update 
+
+df_actual=pd.read_csv("icnf_2021_raw.csv")
+
+# XML Query 
+# Get data
+resp = requests.get(
+    'https://fogos.icnf.pt/localizador/webserviceocorrencias.asp?ANO=2021')
+
+# Parse XML
+et = ET.parse(io.StringIO(resp.text))
+
+# Create DataFrame
+
+df = pd.DataFrame([
+    {f.tag: f.text for f in e.findall('./')} for e in et.findall('./')]
+)
+
+# Append only new records 
+
+df_actual.append(df[df.isin(df_actual) == False])
+
+# Save to CSV 
+
+df_actual.to_csv("icnf_2021_raw.csv")
