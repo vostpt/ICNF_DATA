c9722545ffa2a0d1c5afb5a72cc17dcdb0c2670f
Jorge Gomes <jorge.gomes@vost.pt>
2021-06-10 17:22:18 +0100

    Introducing date based queries
    The first version of this script was not working properly due to, what I think, was a limitation on ICNF's server side. 
    This new version now checks for the current date and retrieves the records from the previous day to the main csv file

--- xmlretriever.py
+++ xmlretriever.py
@@ -10,15 +10,32 @@ import xml.etree.ElementTree as ET
 import io
 from io import StringIO  
 import time 
+import datetime
+
+# Get values for current year, month, and year 
+dt = datetime.datetime.today()
+
+year = dt.year
+month = dt.month
+day = dt.day-1 # Variable day equals the previous day
+
+# Define XML query URL based on date 
+
+url = f"https://fogos.icnf.pt/localizador/webserviceocorrencias.asp" \
+f"?ANO={year}" \
+f"&MES={month}" \
+f"&DIA={day}" \
+
 
 # Get last update 
 
 df_actual=pd.read_csv("icnf_2021_raw.csv")
 
 # XML Query 
+
 # Get data
-resp = requests.get(
-    'https://fogos.icnf.pt/localizador/webserviceocorrencias.asp?ANO=2021&MES=06')
+
+resp = requests.get(url)
 
 # Parse XML
 et = ET.parse(io.StringIO(resp.text))
